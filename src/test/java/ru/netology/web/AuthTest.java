64a04c1d5215002eae20d3a00f1d153b370081af
Elizar Abramov <nezha2077@mail.ru>
2022-06-03 17:44:03 +0700

    error correction

--- src/test/java/ru/netology/web/AuthTest.java
+++ src/test/java/ru/netology/web/AuthTest.java
@@ -1,46 +1,19 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeAll;
+
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.RegistrationInfo;
-import ru.netology.repo.Repository;
 
-import java.util.Objects;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static io.restassured.RestAssured.given;
 
 class AuthTest {
-    private static final Repository repository = new Repository();
-    private static RegistrationInfo first = new RegistrationInfo(DataGenerator.Registration.generateInfo("ru").getLogin(),
-            DataGenerator.Registration.generateInfo("ru").getPassword(), repository.returnStatus());
-
-
-    private static RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
-
-    @BeforeAll
-    static void setUpAll() {
-        repository.add(first);
-        given()
-                .spec(requestSpec)
-                .body(first)
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
-    }
+    private final Faker faker = new Faker((new Locale("ru")));
 
     @BeforeEach
     public void setUp() {
@@ -48,16 +21,38 @@ class AuthTest {
     }
 
     @Test
-    public void shouldLoginSuccessfully() {
-        $("[class='input__control'][name='login']").setValue(first.getLogin());
-        $("[class='input__control'][name='password']").setValue(first.getPassword());
+    void shouldLoginSuccessfullyWithValidAndActive() {
+        RegistrationInfo activeUser = DataGenerator.Registration.generateInfoForActive();
+        $("[class='input__control'][name='login']").setValue(activeUser.getLogin());
+        $("[class='input__control'][name='password']").setValue(activeUser.getPassword());
         $("[data-test-id='action-login']").click();
-        if ((Objects.equals(first.getStatus(), "active"))) {
+        $("[id='root']").shouldHave(Condition.text("Личный кабинет"));
+    }
 
-            $("[id='root']").shouldHave(Condition.text("Личный кабинет"));
+    @Test
+    void shouldReportUserIsBlocked() {
+        RegistrationInfo blockedUser = DataGenerator.Registration.generateInfoForBlocked();
+        $("[class='input__control'][name='login']").setValue(blockedUser.getLogin());
+        $("[class='input__control'][name='password']").setValue(blockedUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[class='notification__content']").shouldHave((Condition.text("Пользователь заблокирован")));
+    }
 
-        } else {
-            $("[class='notification__content']").shouldHave((Condition.text("Пользователь заблокирован")));
-        }
+    @Test
+    void shouldReportWhenInvalidLoginForActive() {
+        RegistrationInfo activeUser = DataGenerator.Registration.generateInfoForActive();
+        $("[class='input__control'][name='login']").setValue((faker.name().firstName()));
+        $("[class='input__control'][name='password']").setValue(activeUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']").shouldHave(Condition.text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void shouldReportWhenInvalidPasswordForActive() {
+        RegistrationInfo activeUser = DataGenerator.Registration.generateInfoForActive();
+        $("[class='input__control'][name='login']").setValue(activeUser.getLogin());
+        $("[class='input__control'][name='password']").setValue(faker.internet().password());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']").shouldHave(Condition.text("Неверно указан логин или пароль"));
     }
 }
--- src/test/java/ru/netology/web/DataGenerator.java
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -1,23 +1,53 @@
 package ru.netology.web;
 
 import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import lombok.experimental.UtilityClass;
 import ru.netology.domain.RegistrationInfo;
-import ru.netology.repo.Repository;
+
 
 import java.util.Locale;
 
+import static io.restassured.RestAssured.given;
+
 @UtilityClass
 public class DataGenerator {
-    private static final Repository repository = new Repository();
+
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    static void setUpAll(RegistrationInfo user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+
 
     @UtilityClass
     public static class Registration {
-        public static RegistrationInfo generateInfo(String locale) {
+        public static RegistrationInfo generateInfoForActive() {
+            Faker faker = new Faker(new Locale("ru"));
+            RegistrationInfo active = new RegistrationInfo(faker.name().firstName(), Integer.toString(faker.number().numberBetween(10000000, 99999999)), "active");
+            setUpAll(active);
+            return active;
+        }
 
-            Faker faker = new Faker(new Locale(locale));
-
-            return new RegistrationInfo(faker.name().firstName(), Integer.toString(faker.number().numberBetween(10000000, 99999999)), repository.returnStatus());
+        public static RegistrationInfo generateInfoForBlocked() {
+            Faker faker = new Faker(new Locale("ru"));
+            RegistrationInfo blocked = new RegistrationInfo(faker.name().firstName(), Integer.toString(faker.number().numberBetween(10000000, 99999999)), "blocked");
+            setUpAll(blocked);
+            return blocked;
         }
     }
 }
